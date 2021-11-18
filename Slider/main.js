469a50d017c06af41d063f767dbf28a4433d80b0
Emanuel Amerio <83380305+emarose@users.noreply.github.com>
2021-11-18 10:00:01 -0300

    Add files via upload

---
+++ Slider/main.js
@@ -0,0 +1,34 @@
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
+const images = document.querySelector('.carousel').children;
+const totalImages = images.length;
+let index = 0;
+
+prev.addEventListener('click', () => {
+  nextImage('next');
+})
+
+next.addEventListener('click', () => {
+  nextImage('prev');
+})
+
+function nextImage(direction) {
+  let i = 0;
+  if(direction == 'next') {
+    index++;
+    if(index == totalImages) {
+      index = 0;
+    }
+  } else {
+    if(index == 0) {
+      index = totalImages - 1;
+    } else {
+      index--;
+    }
+  }
+
+  for(; i < totalImages; i++) {
+    images[i].classList.remove('main'); 
+  }
+  images[index].classList.add('main'); //agrega display Block
+}
